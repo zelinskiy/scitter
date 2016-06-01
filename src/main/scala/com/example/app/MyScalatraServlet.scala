c4074ae98050da1cbc4dceee779a58deb9671fb3
zelinskiy <zelinskiy1917@gmail.com>
2016-06-01 22:19:53 +0300

    Showing posts

--- src/main/scala/com/example/app/MyScalatraServlet.scala
+++ src/main/scala/com/example/app/MyScalatraServlet.scala
@@ -2,15 +2,24 @@ package com.example.app
 
 import org.scalatra._
 
+
+
+
+
+
 class MyScalatraServlet extends HelloWorldAppStack {
+  
+  
 
   get("/") {
     <html>
       <body>
         <h1>eeeeeeee, world!</h1>
-				<a href="post">my servlet</a>.
+				<a href="post">my servlet</a>
       </body>
     </html>
   }
+  
+  
 
 }
--- src/main/scala/com/example/app/PostServlet.scala
+++ src/main/scala/com/example/app/PostServlet.scala
@@ -2,15 +2,46 @@ package com.example.app
 
 import org.scalatra._
 
+import scala.util.{Failure, Success, Try}
+
+
 class PostServlet extends HelloWorldAppStack {
 
+
+  val myposts:List[Post] = List(new Post("post1", 1), new Post("post2", 2))
+  
+  
+  
   get("/") {
-    <html>
-      <body>
-        <h1>Еееееее Скалка</h1>
-        Say <a href="hello-scalate">hello to Scalate</a>.
-      </body>
-    </html>
+    contentType="text/html"
+    jade("/WEB-INF/templates/views/posts.jade",
+      "layout" -> "",
+      "posts" -> myposts
+    )
+  }
+  
+  get("/:id") {
+    Try {
+      params("id").toInt
+    } match {
+      case Success(id) => myposts.find(_.Id == id) match {
+        case Some(post:Post) => post.Location
+        case None => "#"
+      }
+      case Failure(ex) => pass()
+    }
   }
 
 }
+
+
+
+
+class Post(body:String, id: Int){
+  val Id = id
+  val Body = body
+  val Location = "/post/"+ id.toString
+}
+
+
+
